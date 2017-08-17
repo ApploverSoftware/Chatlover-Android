36fe22d512f55d959ded27b43ec76680d25c638e
sp0rk <julian.jurec@gmail.com>
2017-08-17 10:47:17 +0200

    User model changed for consistency

--- firebasechat/src/main/java/pl/applover/firebasechat/model/Channel.kt
+++ firebasechat/src/main/java/pl/applover/firebasechat/model/Channel.kt
@@ -6,13 +6,13 @@ import android.os.Parcelable
 /**
  * Created by sp0rk on 10/08/17.
  */
-class Channel(val id: String, val name: String, private val users: HashMap<String, String>, private val messages: HashMap<String, Message>, val picture: String? = null) : Parcelable {
-    constructor() : this("","", HashMap<String, String>(), HashMap<String, Message>()) //needed for Firebase
+class Channel(val id: String, val name: String, val users: HashMap<String, HashMap<String, String>>, val messages: HashMap<String, Message>, val picture: String? = null) : Parcelable {
+    constructor() : this("", "", HashMap<String, HashMap<String, String>>(), HashMap<String, Message>()) //needed for Firebase
     //Parcelable implementation
     constructor(parcel: Parcel) : this(
             id = parcel.readString(),
             name = parcel.readString(),
-            users = HashMap<String, String>(),
+            users = HashMap<String, HashMap<String, String>>(),
             messages = HashMap<String, Message>(),
             picture = parcel.readString()) {
         parcel.readMap(users, String::class.java.classLoader)
@@ -20,10 +20,10 @@ class Channel(val id: String, val name: String, private val users: HashMap<Strin
     }
 
     var userList = emptyList<String>()
-        get() = users.values.toList()
+        get() = users.values.map { it["uid"]?:"" }
 
     var messageList = emptyList<Message>()
-        get() =  messages.entries.sortedBy { it.key }.map { it.value }
+        get() = messages.entries.sortedBy { it.key }.map { it.value }
 
     override fun writeToParcel(dest: Parcel?, flags: Int) {
         dest?.writeString(id)
@@ -32,9 +32,12 @@ class Channel(val id: String, val name: String, private val users: HashMap<Strin
         dest?.writeMap(messages)
         dest?.writeString(picture)
     }
+
     override fun describeContents() = 0
+
     companion object CREATOR : Parcelable.Creator<Channel> {
         override fun createFromParcel(p: Parcel) = Channel(p)
         override fun newArray(s: Int): Array<Channel?> = arrayOfNulls(s)
     }
+
 }
